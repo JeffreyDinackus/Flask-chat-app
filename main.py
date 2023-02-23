091fae31251f9fbc9aa053e17a1231b11f018514
JeffreyDinackus <z3L9m@protonmail.com>
2023-02-22 22:59:34 -0500

    made the app

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+#change later
+app.config['SECRET'] = "secretisnothere"
+socketio = SocketIO(app cors_allowed_origins="*")
+
+
+socketio.on('message')
+def handle_message(message):
+    print("Recieved message: " + message)
+    if message != "User Connected!":
+        send(message, boardcast=True)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="localhost")
